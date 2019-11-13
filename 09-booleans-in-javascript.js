52bbe33490b7764089aeb84ca4b406ce8d231ee4
Martin Fencl <martin.fencl@gmail.com>
2019-11-13 09:45:49 -0600

    add an example and list truthy

--- 09-booleans-in-javascript.js
+++ 09-booleans-in-javascript.js
@@ -7,6 +7,7 @@ let example51 = ' ';
 let example6 = NaN;
 let example7 = -5;
 let example8 = 0;
+let example9 = [];
 
 // console.log(Boolean(example1));
 // console.log(Boolean(example2));
@@ -17,3 +18,10 @@ let example8 = 0;
 // console.log(Boolean(example6));
 // console.log(Boolean(example7));
 // console.log(Boolean(example8));
+// console.log(Boolean(example9));
+
+// truthy: 
+//  true
+//  non-empty string
+//  non-zero number 
+//  array (even empty)
